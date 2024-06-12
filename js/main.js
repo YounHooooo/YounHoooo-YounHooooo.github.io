0139d1e486ab91190f8224fb4202f3f498fe430b
YounHooooo <yh6316l@naver.com>
2024-06-12 23:58:00 +0900

    intersectionObserver

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ function init() {
   $navLinkList.forEach((el) => el.addEventListener('click', toggleMenu));
 }
 init();
+
+const options = {
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const sectionId = entry.target.id;
+    if (entry.isIntersecting) {
+      document
+        .querySelector(`.nav__link[href*=${sectionId}]`)
+        .classList.add('active-link');
+      // entry.target.classList.add('bg-dark');
+    } else {
+      // entry.target.classList.remove('bg-dark');
+    }
+  });
+}, options);
+
+const $sectionList = document.querySelectorAll('.section');
+$sectionList.forEach((el) => observer.observe(el));
